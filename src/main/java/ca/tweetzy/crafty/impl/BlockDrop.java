4a1e5458b2fdd0cdf7a2ba51330fc8a7760ab4db
Kiran Hart <kiran.hart@live.com>
2024-04-19 16:46:05 -0400

    🔨 pass condition into mob/block drops
    Took 16 seconds

--- src/main/java/ca/tweetzy/crafty/impl/BlockDrop.java
+++ src/main/java/ca/tweetzy/crafty/impl/BlockDrop.java
@@ -1,5 +1,6 @@
 package ca.tweetzy.crafty.impl;
 
+import ca.tweetzy.crafty.api.drop.Condition;
 import ca.tweetzy.crafty.api.drop.Drop;
 import ca.tweetzy.flight.comp.enums.CompMaterial;
 import lombok.Getter;
@@ -19,23 +20,25 @@ public final class BlockDrop extends Drop {
 	private CompMaterial parentBlock;
 	private boolean dropOnNatural;
 	private boolean dropOnPlaced;
+	private Condition condition;
 
-	public BlockDrop(@NonNull final UUID id, CompMaterial parentBlock, @NonNull final ItemStack item, final double chance, boolean dropOnNatural, boolean dropOnPlaced, List<String> commands) {
-		super(id, DropType.BLOCK, item, chance, commands);
+	public BlockDrop(@NonNull final UUID id, CompMaterial parentBlock, @NonNull final ItemStack item, final double chance, boolean dropOnNatural, boolean dropOnPlaced, List<String> commands, Condition condition) {
+		super(id, DropType.BLOCK, item, chance, commands, condition);
 		this.dropOnNatural = dropOnNatural;
 		this.dropOnPlaced = dropOnPlaced;
 		this.parentBlock = parentBlock;
+		this.condition = condition;
 	}
 
 	public BlockDrop(@NonNull final UUID id, CompMaterial parentBlock, @NonNull final ItemStack item, final double chance, boolean dropOnNatural, boolean dropOnPlaced) {
-		this(id, parentBlock, item, chance, dropOnNatural, dropOnPlaced, new ArrayList<>());
+		this(id, parentBlock, item, chance, dropOnNatural, dropOnPlaced, new ArrayList<>(), DropCondition.template());
 	}
 
 	public BlockDrop(@NonNull final UUID id, CompMaterial parentBlock, @NonNull final ItemStack item, final double chance) {
-		this(id, parentBlock, item, chance, true, false, new ArrayList<>());
+		this(id, parentBlock, item, chance, true, false, new ArrayList<>(), DropCondition.template());
 	}
 
 	public static BlockDrop empty(CompMaterial parentBlock) {
-		return new BlockDrop(UUID.randomUUID(), parentBlock, CompMaterial.STONE.parseItem(), 50, true, true, new ArrayList<>());
+		return new BlockDrop(UUID.randomUUID(), parentBlock, CompMaterial.STONE.parseItem(), 50, true, true, new ArrayList<>(), DropCondition.template());
 	}
 }
--- src/main/java/ca/tweetzy/crafty/impl/MobDrop.java
+++ src/main/java/ca/tweetzy/crafty/impl/MobDrop.java
@@ -1,5 +1,6 @@
 package ca.tweetzy.crafty.impl;
 
+import ca.tweetzy.crafty.api.drop.Condition;
 import ca.tweetzy.crafty.api.drop.Drop;
 import ca.tweetzy.flight.comp.enums.CompMaterial;
 import lombok.Getter;
@@ -21,24 +22,26 @@ public final class MobDrop extends Drop {
 	private boolean dropOnNatural;
 	private boolean dropFromSpawner;
 	private boolean dropFromEgg;
+	private Condition condition;
 
-	public MobDrop(@NonNull final UUID id, EntityType parentMob, @NonNull final ItemStack item, final double chance, List<String> commands, boolean dropOnNatural, boolean dropFromSpawner, boolean dropFromEgg) {
-		super(id, DropType.MOB, item, chance, commands);
+	public MobDrop(@NonNull final UUID id, EntityType parentMob, @NonNull final ItemStack item, final double chance, List<String> commands, boolean dropOnNatural, boolean dropFromSpawner, boolean dropFromEgg, Condition condition) {
+		super(id, DropType.MOB, item, chance, commands,condition);
 		this.parentMob = parentMob;
 		this.dropOnNatural = dropOnNatural;
 		this.dropFromSpawner = dropFromSpawner;
 		this.dropFromEgg = dropFromEgg;
+		this.condition = condition;
 	}
 
 	public MobDrop(@NonNull final UUID id, EntityType parentMob, @NonNull final ItemStack item, final double chance, boolean dropOnNatural, boolean dropFromSpawner, boolean dropFromEgg) {
-		this(id, parentMob, item, chance, new ArrayList<>(), dropOnNatural, dropFromSpawner, dropFromEgg);
+		this(id, parentMob, item, chance, new ArrayList<>(), dropOnNatural, dropFromSpawner, dropFromEgg, DropCondition.template());
 	}
 
 	public MobDrop(@NonNull final UUID id, EntityType parentMob, @NonNull final ItemStack item, final double chance) {
-		this(id, parentMob, item, chance, new ArrayList<>(), true, true, true);
+		this(id, parentMob, item, chance, new ArrayList<>(), true, true, true, DropCondition.template());
 	}
 
 	public static MobDrop empty(EntityType parentMob) {
-		return new MobDrop(UUID.randomUUID(), parentMob, CompMaterial.STONE.parseItem(), 50, new ArrayList<>(), true, true, true);
+		return new MobDrop(UUID.randomUUID(), parentMob, CompMaterial.STONE.parseItem(), 50, new ArrayList<>(), true, true, true, DropCondition.template());
 	}
 }
